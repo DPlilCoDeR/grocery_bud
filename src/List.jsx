66b763c6e1e6d4aad88d7cb52c7f30b75bc089e0
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-03-15 23:44:59 +0100

    add icons of edit and trash

--- src/List.jsx
+++ src/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
+import { FaEdit, FaTablet, FaTableTennis, FaTrash } from 'react-icons/fa'
 const List = ({items}) => {
 
   return (
@@ -8,6 +8,14 @@ const List = ({items}) => {
         const {id, title} = item;
         return <article key={id} className='grocery-item'>
             <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button type='button' className='edit-btn'>
+                <FaEdit/>
+              </button>
+              <button type='button' className='delete-btn'>
+                <FaTrash/>
+              </button>
+            </div>
           </article>
       })}
     </div>
